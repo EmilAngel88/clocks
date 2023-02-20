9d633ac0089f0a7966ad6e6567c1586d56f9c8bf
emil <e.baraboshkin@idaproject.com>
2023-02-20 21:20:02 +0300

    tic-tac

--- src/index.js
+++ src/index.js
@@ -1,17 +1,43 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+const clock = {
+    h: null,
+    m: null,
+    s: null,
+}
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+function init(){
+    setInterval(handleCurrentTime, 1000);
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function handleCurrentTime() {
+    const currentTime = new Date()
+
+    const hours = currentTime.getHours()
+    const minutes = currentTime.getMinutes()
+    const seconds = currentTime.getSeconds()
+
+    updateState(hours, minutes, seconds)
+}
+
+function updateState(hours, minutes, seconds) {
+    const timeAngle = {
+        h: hours < 12 ? hours : (hours - 12) * 30,
+        m: minutes * 6,
+        s: seconds * 6,
+    }
+
+    this.clock = timeAngle
+
+    onUpdate()
+}
+
+function onUpdate() {
+    let h = document.getElementById('hours').style
+    let m = document.getElementById('minutes').style
+    let s = document.getElementById('seconds').style
+
+    h.transform = `rotate(${this.clock.h}deg)`
+    m.transform = `rotate(${this.clock.m}deg)`
+    s.transform = `rotate(${this.clock.s}deg)`
+}
+
+init()
