aafb50c1c6f0c6010049a92a2165918fd87c4f95
emil <e.baraboshkin@idaproject.com>
2023-02-20 21:42:18 +0300

    fix any bags

--- src/index.js
+++ src/index.js
@@ -19,8 +19,9 @@ function handleCurrentTime() {
 }
 
 function updateState(hours, minutes, seconds) {
+    const intermediateHourAngle = Math.floor(minutes / 12) * 6
     const timeAngle = {
-        h: hours < 12 ? hours : (hours - 12) * 30,
+        h: hours < 12 ? (hours * 30 + intermediateHourAngle) : ((hours - 12) * 30 + intermediateHourAngle),
         m: minutes * 6,
         s: seconds * 6,
     }
@@ -38,6 +39,8 @@ function onUpdate() {
     h.transform = `rotate(${this.clock.h}deg)`
     m.transform = `rotate(${this.clock.m}deg)`
     s.transform = `rotate(${this.clock.s}deg)`
+
+    console.log(this.clock)
 }
 
 init()
